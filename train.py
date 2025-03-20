0154161b029ab278a3687a77fa5c8baaec7594d2
DenBuzz <trey@wagerfamily.com>
2025-03-19 20:53:34 -0600

    iter stopper

--- train.py
+++ train.py
@@ -1,25 +1,28 @@
 from omegaconf import DictConfig
 import hydra
 from ray.rllib.algorithms import PPOConfig
-from ray import tune
+from ray.tune import Tuner, RunConfig, TuneConfig, CheckpointConfig
+from ray.tune.stopper import MaximumIterationStopper
 from conf.algorithm import build_config
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="train")
 def main(hydra_cfg: DictConfig):
     algo_config = build_config(PPOConfig, hydra_cfg.algorithm)
-    tuner = tune.Tuner(
+    tuner = Tuner(
         algo_config.algo_class,
         param_space=algo_config,
-        run_config=tune.RunConfig(
+        run_config=RunConfig(
             name="denbot_1on0",
             storage_path="~/src/denbot-rl/ray_results",
-            checkpoint_config=tune.CheckpointConfig(
+            checkpoint_config=CheckpointConfig(
                 num_to_keep=5,
                 checkpoint_frequency=100,
                 checkpoint_at_end=True,
             ),
+            stop=MaximumIterationStopper(max_iter=10_000),
         ),
+        tune_config=TuneConfig(num_samples=1),
     )
 
     return tuner.fit()
